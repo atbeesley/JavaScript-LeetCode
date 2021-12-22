9a1cc9a66267dd9eb66f205f0236f3483ab0a1f7
atbeesley <alexander.beesley@gmail.com>
2021-12-22 07:05:06 +0000

    adds valid_parentheses (initial)

---
+++ valid_parentheses.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+    if(s.includes("(") && s.includes(")")){
+        return true;
+    } else if(s.includes("[") && s.includes("]")){
+        return true;
+    } else if(s.includes("{") && s.includes("}")){
+        return true;
+    } else {
+        return false;
+    }
+};

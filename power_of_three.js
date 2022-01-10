880775f158ffe4251621335b55932bcac3d413bd
atbeesley <alexander.beesley@gmail.com>
2022-01-10 18:17:58 +0000

    adds reverse vowels of a string

--- power_of_three.js
+++ power_of_three.js
@@ -5,3 +5,12 @@
 var isPowerOfThree = function(n) {
     return n===1 || n !== 0 && n % 3 === 0 && isPowerOfThree(n/3);
 };
+
+console.log(isPowerOfThree(45)
+)
+console.log(isPowerOfThree(9)
+)
+console.log(isPowerOfThree(27)
+)
+console.log(isPowerOfThree(36)
+)

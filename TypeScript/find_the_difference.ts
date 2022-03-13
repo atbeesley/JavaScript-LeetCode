01a956e6dd76aa121e31d201ea7fa47a5c9f52be
atbeesley <alexander.beesley@gmail.com>
2022-03-13 10:40:53 +0000

    adds find_the_difference.ts

---
+++ TypeScript/find_the_difference.ts
@@ -0,0 +1,24 @@
+const findTheDifference = (s: string, t: string) => {
+    
+    let obj = {}
+    let array = s.split("")
+
+    array.forEach((element) => {
+        if(obj[element]){
+            obj[element] ++
+        } else {
+            obj[element] = 1
+        } 
+    })
+    
+    for(var i = 0; i < t.length; i++) {
+        if(!obj[t.charAt(i)]){
+            return t.charAt(i)
+        } else {
+            obj[t.charAt(i)]--
+        }
+        if(obj[t.charAt(i)] < 0){
+            return t.charAt(i)
+        }
+    }
+}

a07778d7f30fb2dc5e48df6df3b50896d71e841a
atbeesley <alexander.beesley@gmail.com>
2022-07-22 08:41:12 +0100

    add Power_of_Three.ts

--- TypeScript/Power_of_Three.ts
+++ TypeScript/Power_of_Three.ts
@@ -1,3 +1,3 @@
 function isPowerOfThree(n: number): boolean {
-        return n===1 || n > 0 && n % 3 === 0 && isPowerOfThree(n/3);
+    return n===1 || n > 0 && n % 3 === 0 && isPowerOfThree(n/3);
 };

fa8273940fddc2ddbc35be4211abc152d80ea874
atbeesley <alexander.beesley@gmail.com>
2022-11-16 09:59:57 +0000

    Add Convert_the_Temperature.js

---
+++ JavaScript/Convert_the_Temperature.js
@@ -0,0 +1,7 @@
+var convertTemperature = function(celsius) {
+    let arr = [];
+    const kelvinTemp = celsius + 273.15;
+    const fahrenheitTemp = celsius * 1.80 + 32.00;
+    arr.push(kelvinTemp, fahrenheitTemp);
+    return arr;
+};

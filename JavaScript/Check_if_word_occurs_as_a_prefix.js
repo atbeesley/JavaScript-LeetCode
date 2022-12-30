532135b7de8c6e3d3af76c042352f1315575c9fc
atbeesley <alexander.beesley@gmail.com>
2022-12-30 09:30:42 -0600

    Refactor Check_if_word_occurs_as_a_prefix.js

--- JavaScript/Check_if_word_occurs_as_a_prefix.js
+++ JavaScript/Check_if_word_occurs_as_a_prefix.js
@@ -1,10 +1,10 @@
 var isPrefixOfWord = function(sentence, searchWord) {
     sentence = sentence.split(" ");
     for(let i of sentence){
-        if(i.startsWith(searchWord) === true){
-            let thisIsSilly = sentence.indexOf(i);
-            thisIsSilly = thisIsSilly + 1;
-            return thisIsSilly;
+        if(i.startsWith(searchWord)){
+            let indexOfPrefix = sentence.indexOf(i);
+            indexOfPrefix += 1;
+            return indexOfPrefix;
         } 
     }
     return -1;

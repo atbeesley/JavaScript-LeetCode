f985848334dfbc0aa394a789c23c56cce0b6551b
atbeesley <alexander.beesley@gmail.com>
2023-01-21 12:55:47 +0000

    Maximum_count_of_positive_and_negative_integer.ts

---
+++ TypeScript/Maximum_count_of_positive_and_negative_integer.ts
@@ -0,0 +1,14 @@
+function maximumCount(nums: number[]): number {
+    let pos = 0;
+    let neg = 0;
+
+    for(let i of nums){
+        if(i>0){
+            pos += 1;
+        }else if(i<0){
+            neg += 1;
+        }
+    }
+
+    return Math.max(pos, neg);
+};

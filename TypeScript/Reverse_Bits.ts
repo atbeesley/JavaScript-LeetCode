c4d7feba60e370084093857d1211bdaef117e3bf
atbeesley <alexander.beesley@gmail.com>
2023-03-16 09:50:38 +0100

    Add Reverse_Bits.ts

---
+++ TypeScript/Reverse_Bits.ts
@@ -0,0 +1,6 @@
+function reverseBits(n: number): number {
+    let reversed = n.toString(2);
+    const padding = "0";
+    reversed = padding.repeat(32 - reversed.length) + reversed;
+    return parseInt(reversed.split('').reverse().join(''), 2);
+};
